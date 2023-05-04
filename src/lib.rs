0cd7d3514a204640c46e6a3a54ddd4c67cc32290
Baad C0de <baadc0de@proton.me>
2023-05-04 15:08:30 +0100

    Added window.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
-pub fn run() {
-    println!("Hello, world!");
+use thiserror::Error;
+use winit::{
+    dpi::PhysicalSize,
+    error::OsError,
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+#[derive(Debug, Error)]
+pub enum MageError {
+    #[error("unable to open window")]
+    WindowError(#[from] OsError),
+}
+
+pub fn run() -> Result<(), MageError> {
+    let event_loop = EventLoop::new();
+
+    let window = WindowBuilder::new()
+        .with_inner_size(PhysicalSize::new(640, 480))
+        .with_title("Mage Game")
+        .build(&event_loop)?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            Event::WindowEvent { window_id, event } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                _ => (),
+            },
+            _ => (),
+        }
+    });
 }
